d13ad4db8541451407a8a383013f4d6e22939eb7
akinghill <austinkinghill@gmail.com>
2020-12-21 14:09:19 -0700

    deploy prep

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,11 +1,9 @@
 import React from 'react';
 import { makeStyles } from '@material-ui/core/styles';
-import AppBar from '@material-ui/core/AppBar';
-import Toolbar from '@material-ui/core/Toolbar';
-import Typography from '@material-ui/core/Typography';
-import Button from '@material-ui/core/Button';
-import IconButton from '@material-ui/core/IconButton';
+import { AppBar, Toolbar, Typography, Button, IconButton } from '@material-ui/core/';
 import MenuIcon from '@material-ui/icons/Menu';
+import { useTheme } from '@material-ui/core/styles';
+import useMediaQuery from '@material-ui/core/useMediaQuery';
 
 const useStyles = makeStyles((theme) => ({
   root: {
@@ -21,6 +19,8 @@ const useStyles = makeStyles((theme) => ({
 
 export default function Navbar() {
   const classes = useStyles();
+  const theme = useTheme();
+  const matches = useMediaQuery(theme.breakpoints.down('sm'));
 
   return (
     <div className={classes.root}>
@@ -32,9 +32,17 @@ export default function Navbar() {
           <Typography color="secondary" variant="h6" className={classes.title}>
             Teach You Freedom
           </Typography>
-          <Button color="secondary">Break Free</Button>
-          <Button color="secondary">About</Button>
-          <Button color="secondary">Login</Button>
+          {
+            !matches &&
+            (
+              <>
+                <Button color="secondary">Break Free</Button>
+                <Button color="secondary">About</Button>
+                <Button color="secondary">Login</Button>
+              </>
+            )
+          }
+
         </Toolbar>
       </AppBar>
     </div>
--- src/views/HomePage/sections/RecentPosts.jsx
+++ src/views/HomePage/sections/RecentPosts.jsx
@@ -22,13 +22,18 @@ const useStyles = makeStyles({
   },
   postTitle: {
     alignSelf: 'flex-start',
-    textDecoration: "underline"
   },
   frame: {
     border: "0px",
     width: '355px',
     height: "200px",
-    marginBottom: "1rem"
+    marginBottom: "5px"
+  },
+  date: {
+    fontFamily: 'sans-serif',
+    alignSelf: 'flex-end',
+    margin: '0px',
+    marginBottom: "5px"
   }
 });
 
@@ -40,26 +45,30 @@ const RecentPosts = () => {
       <div className={classes.postSection}>
         <div>
           <Typography className={classes.recentsTitle} align='center' variant='h2'> RecentPosts </Typography>
-          <Grid container spacing={3} className={classes.gridContainer}>
-            <Grid item sm={12} md={4}>
+          <Grid container spacing={3} className={classes.gridContainer} justify="center">
+            <Grid item sm={12} md={3}>
               <Paper className={classes.paperCard} elevation={3}>
               <iframe className={classes.frame} title="Recent Post 1" src="https://www.youtube.com/embed/jtEw9FcuQuk" frameBorder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowFullScreen></iframe>
-                <Typography variant='h5' className={classes.postTitle}>Increase Productivity NOW</Typography>
+                <p className={classes.date}>12-19-20</p>
+                <Typography variant='h5' className={classes.postTitle}>Increase Productivity Now</Typography>
               </Paper>
             </Grid>
-            <Grid item sm={12} md={4}>
+
+            <Grid item sm={12} md={3}>
               <Paper className={classes.paperCard} elevation={3}>
-              <iframe className={classes.frame} title="Recent Post 1" src="https://www.youtube.com/embed/jtEw9FcuQuk" frameBorder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowFullScreen></iframe>
-                <Typography variant='h5' className={classes.postTitle}>Increase Productivity NOW</Typography>
-              </Paper>
-            </Grid>
-            <Grid item sm={12} md={4}>
-              <Paper className={classes.paperCard} elevation={3}>
-              <iframe className={classes.frame} title="Recent Post 1" src="https://www.youtube.com/embed/jtEw9FcuQuk" frameBorder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowFullScreen></iframe>
-                <Typography variant='h5' className={classes.postTitle}>Increase Productivity NOW</Typography>
+              <iframe className={classes.frame} title="Recent Post 1" src="https://www.youtube.com/embed/r4WDyDTPIgY" frameBorder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowFullScreen></iframe>
+                <p className={classes.date}>12-19-20</p>
+                <Typography variant='h5' className={classes.postTitle}>Why You Can't Change</Typography>
               </Paper>
             </Grid>
       
+            <Grid item sm={12} md={3}>
+              <Paper className={classes.paperCard} elevation={3}>
+              <iframe className={classes.frame} title="Recent Post 1" src="https://www.youtube.com/embed/nh1x33ZrT0M" frameBorder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowFullScreen></iframe>
+                <p className={classes.date}>11-14-20</p>
+                <Typography variant='h5' className={classes.postTitle}>Start Before You're Ready</Typography>
+              </Paper>
+            </Grid>
           </Grid>
         </div>
       </div>
