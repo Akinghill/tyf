4e41779c4d3c6117d254b58851f30e421aade722
akinghill <austinkinghill@gmail.com>
2021-01-02 11:33:23 -0700

    add routing

--- src/App.js
+++ src/App.js
@@ -1,15 +1,18 @@
 import React from 'react'
 import { ThemeProvider } from '@material-ui/core'
+import { Switch, Route } from 'react-router-dom'
 
-import theme from './theme.js'
+import theme from './theme/theme.js'
 import HomePage from './views/HomePage/HomePage'
+import BreakFreePage from './views/BreakFree/BreakFreePage.jsx'
 
 function App() {
   return (
     <ThemeProvider theme={theme}>
-      <div className="App">
-        <HomePage/>
-      </div>
+      <Switch>
+        <Route exact path='/' component={HomePage} />
+        <Route exact path='/breakfree' component={BreakFreePage} />
+      </Switch>
     </ThemeProvider>
   );
 }
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,9 +1,15 @@
 import React from 'react';
 import { makeStyles } from '@material-ui/core/styles';
-import { AppBar, Toolbar, Typography, Button, IconButton } from '@material-ui/core/';
+import { AppBar, Divider, List, ListItem, Drawer, Toolbar, Typography, Button, IconButton, ListItemText, ListItemIcon, useMediaQuery } from '@material-ui/core/';
 import MenuIcon from '@material-ui/icons/Menu';
 import { useTheme } from '@material-ui/core/styles';
-import useMediaQuery from '@material-ui/core/useMediaQuery';
+import ChevronLeftIcon from '@material-ui/icons/ChevronLeft';
+import ChevronRightIcon from '@material-ui/icons/ChevronRight';
+import Brightness7Icon from '@material-ui/icons/Brightness7';
+import AccountBoxIcon from '@material-ui/icons/AccountBox';
+import InfoIcon from '@material-ui/icons/Info';
+
+import {Link} from 'react-router-dom'
 
 const useStyles = makeStyles((theme) => ({
   root: {
@@ -15,18 +21,43 @@ const useStyles = makeStyles((theme) => ({
   title: {
     flexGrow: 1,
   },
+  closeIcon: {
+    color: "white"
+  },
+  drawerPaper: {
+    background: "#2a2929",
+    color: "white"
+  },
+  menuIcon: {
+    color: "white"
+  }
 }));
 
 export default function Navbar() {
   const classes = useStyles();
   const theme = useTheme();
   const smScreen = useMediaQuery(theme.breakpoints.down('sm'));
+  const [open, setOpen] = React.useState(false);
+
+  const handleDrawerOpen = () => {
+    setOpen(true);
+  };
+
+  const handleDrawerClose = () => {
+    setOpen(false);
+  };
 
   return (
     <div className={classes.root}>
       <AppBar position="fixed">
         <Toolbar>
-          <IconButton edge="start" className={classes.menuButton} color="inherit" aria-label="menu">
+          <IconButton
+            onClick={handleDrawerOpen}
+            edge="start"
+            className={classes.menuButton}
+            color="secondary"
+            aria-label="menu"
+          >
             <MenuIcon />
           </IconButton>
           <Typography color="secondary" variant="h6" className={classes.title}>
@@ -36,7 +67,9 @@ export default function Navbar() {
             !smScreen &&
             (
               <>
-                <Button color="secondary">Break Free</Button>
+                <Link to='/breakfree'>
+                  <Button color="secondary">Break Free</Button>
+                </Link>
                 <Button color="secondary">About</Button>
                 <Button color="secondary">Login</Button>
               </>
@@ -44,6 +77,35 @@ export default function Navbar() {
           }
         </Toolbar>
       </AppBar>
+      <Drawer
+        className={classes.drawer}
+        variant="persistent"
+        anchor="left"
+        open={open}
+        classes={{
+          paper: classes.drawerPaper,
+        }}
+      >
+        <div className={classes.drawerHeader}>
+          <IconButton className={classes.closeIcon} onClick={handleDrawerClose}>
+            {theme.direction === 'ltr' ? <ChevronLeftIcon /> : <ChevronRightIcon />}
+          </IconButton>
+        </div>
+        <Divider />
+        <List>
+          {['Login', 'Break Free', 'About'].map((text, index) => (
+            <ListItem button key={text}>
+              <ListItemIcon classList>
+                {index === 0 && <AccountBoxIcon className={classes.menuIcon} />}
+                {index === 1 && <Brightness7Icon className={classes.menuIcon} />}
+                {index === 2 && <InfoIcon className={classes.menuIcon} />}
+              </ListItemIcon>
+              <ListItemText primary={text} />
+            </ListItem>
+          ))}
+        </List>
+        <Divider />
+      </Drawer>
     </div>
   );
 }
