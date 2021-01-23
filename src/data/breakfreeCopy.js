475e3ea6683f76d999faf2b323ef9f74ddd728d4
akinghill <austinkinghill@gmail.com>
2021-01-23 12:17:24 -0700

    assets

---
+++ src/data/breakfreeCopy.js
@@ -0,0 +1,6 @@
+export const bfCopy = {
+  intro:
+    'The process of removing mental and emotional barriers keeping you from your authentic self and allowing you to create a compelling future',
+  subIntro: 
+    'We all have dreams, but we\'ve lost so much certainty'
+};
