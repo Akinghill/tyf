dae1c0930f7e1678bc32637dcb5ab64347011f3a
akinghill <austinkinghill@gmail.com>
2021-01-23 12:18:13 -0700

    navbar toggle and extras

--- src/GlobalStyles.js
+++ src/GlobalStyles.js
@@ -1,13 +1,8 @@
 import { createGlobalStyle } from 'styled-components'
 
 export const GlobalStyle = createGlobalStyle`
-  * {
-    box-sizing: border-box;
-  }
-
   body {
   }
-
   a {
     text-decoration: none;
   }
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,19 +1,41 @@
 import React from 'react';
 import { makeStyles } from '@material-ui/core/styles';
-import { AppBar, Divider, List, ListItem, Drawer, Toolbar, Typography, Button, IconButton, ListItemText, ListItemIcon, useMediaQuery } from '@material-ui/core/';
+import {
+  AppBar,
+  Divider,
+  List,
+  ListItem,
+  Drawer,
+  Toolbar,
+  Button,
+  IconButton,
+  ListItemText,
+  ListItemIcon,
+  useMediaQuery,
+} from '@material-ui/core/';
 import MenuIcon from '@material-ui/icons/Menu';
 import { useTheme } from '@material-ui/core/styles';
-import ChevronLeftIcon from '@material-ui/icons/ChevronLeft';
 import ChevronRightIcon from '@material-ui/icons/ChevronRight';
 import Brightness7Icon from '@material-ui/icons/Brightness7';
 import AccountBoxIcon from '@material-ui/icons/AccountBox';
 import InfoIcon from '@material-ui/icons/Info';
 
-import {Link} from 'react-router-dom'
+import { Link } from 'react-router-dom';
+import logo from '../assets/logo.png';
+import logoIcon from '../assets/logo-icon.png';
+
+
 
 const useStyles = makeStyles((theme) => ({
   root: {
     flexGrow: 1,
+  },
+  logo: {
+    maxWidth: 160,
+  },
+  logoIcon: {
+    maxWidth: 70,
+    maxHeight: 70,
   },
   menuButton: {
     marginRight: theme.spacing(2),
@@ -22,21 +44,26 @@ const useStyles = makeStyles((theme) => ({
     flexGrow: 1,
   },
   closeIcon: {
-    color: "white"
+    color: 'white',
   },
   drawerPaper: {
-    background: "#2a2929",
-    color: "white"
+    background: '#2a2929',
+    color: 'white',
   },
   menuIcon: {
-    color: "white"
-  }
+    color: 'white',
+  },
+  mobileNav: {
+    flex: 1,
+    display: 'flex',
+    justifyContent: 'space-between',
+  },
 }));
 
 export default function Navbar() {
   const classes = useStyles();
   const theme = useTheme();
-  const smScreen = useMediaQuery(theme.breakpoints.down('sm'));
+  const smScreen = useMediaQuery(theme.breakpoints.down('xs'));
   const [open, setOpen] = React.useState(false);
 
   const handleDrawerOpen = () => {
@@ -51,36 +78,47 @@ export default function Navbar() {
     <div className={classes.root}>
       <AppBar position="fixed">
         <Toolbar>
-          <IconButton
-            onClick={handleDrawerOpen}
-            edge="start"
-            className={classes.menuButton}
-            color="secondary"
-            aria-label="menu"
-          >
-            <MenuIcon />
-          </IconButton>
-          <Typography color="secondary" variant="h6" className={classes.title}>
-            Teach You Freedom
-          </Typography>
-          {
-            !smScreen &&
-            (
-              <>
-                <Link to='/breakfree'>
-                  <Button color="secondary">Break Free</Button>
+          {smScreen ? (
+            <>
+              <div className={classes.mobileNav}>
+                <Link to="/">
+                  <img src={logoIcon} alt="logo" className={classes.logoIcon} />
                 </Link>
-                <Button color="secondary">About</Button>
-                <Button color="secondary">Login</Button>
-              </>
-            )
-          }
+                <IconButton
+                  onClick={handleDrawerOpen}
+                  edge="start"
+                  className={classes.menuButton}
+                  color="secondary"
+                  aria-label="menu"
+                >
+                  <MenuIcon />
+                </IconButton>
+              </div>
+            </>
+          ) : (
+            <>
+              <Link to="/">
+                <img src={logo} alt="logo" className={classes.logo} />
+              </Link>
+              <div className={classes.title}></div>
+            </>
+          )}
+
+          {!smScreen && (
+            <>
+              <Link to="/breakfree">
+                <Button color="secondary">Break Free</Button>
+              </Link>
+              <Button color="secondary">About</Button>
+              <Button color="secondary">Login</Button>
+            </>
+          )}
         </Toolbar>
       </AppBar>
       <Drawer
         className={classes.drawer}
         variant="persistent"
-        anchor="left"
+        anchor="right"
         open={open}
         classes={{
           paper: classes.drawerPaper,
@@ -88,16 +126,18 @@ export default function Navbar() {
       >
         <div className={classes.drawerHeader}>
           <IconButton className={classes.closeIcon} onClick={handleDrawerClose}>
-            {theme.direction === 'ltr' ? <ChevronLeftIcon /> : <ChevronRightIcon />}
+            <ChevronRightIcon />
           </IconButton>
         </div>
         <Divider />
         <List>
           {['Login', 'Break Free', 'About'].map((text, index) => (
             <ListItem button key={text}>
-              <ListItemIcon classList>
+              <ListItemIcon>
                 {index === 0 && <AccountBoxIcon className={classes.menuIcon} />}
-                {index === 1 && <Brightness7Icon className={classes.menuIcon} />}
+                {index === 1 && (
+                  <Brightness7Icon className={classes.menuIcon} />
+                )}
                 {index === 2 && <InfoIcon className={classes.menuIcon} />}
               </ListItemIcon>
               <ListItemText primary={text} />
